bac1320e045f4d9854d16cfb4e10c9c089b4ec65
Igor Oliveira da Silva <x523726@rede.sp>
2025-02-24 11:56:29 -0300

    003

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
 function Footer() {
 	return (
 		<footer>
-			{/* <p>© 2025 Academia IronFit</p> */}
+			<p>© 2025 Academia IronFit</p>
+			<p>© 2025 Academia IronFit</p>
+			<p>© 2025 Academia IronFit</p>
+			<p>© 2025 Academia IronFit</p>
+			<p>© 2025 Academia IronFit</p>
+			<p>© 2025 Academia IronFit</p>
+			<p>© 2025 Academia IronFit</p>
 		</footer>
 	);
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+
 import "../css/pages/Home/home.css";
 
 function Home() {
@@ -25,56 +27,90 @@ function Home() {
       </section>
 
       <section className="mais_sobre_nos">
-        <div className="div_textp_sobre_nos">
+        <div className="div_texto_sobre_nos">
           <h2 className="titulo_sobre_nos">Por que escolher nossa academia?</h2>
           <p className="paragrafo_sobre_nos">
             Descubra o que torna nossa unidade a escolha ideal para uma vida mais saudável.
           </p>
         </div>
 
-        <Swiper
-          modules={[Navigation, Pagination, Autoplay]}
-          spaceBetween={25}
-          slidesPerView={1}
-          autoplay={{ delay: 5000 }}
-          loop={true}
-          centeredSlides={true}
-          className="carrossel"
-        >
-          <SwiperSlide>
-            <div className="bloco_mais_sobre">
-              <img className="img_icon_mais_sobre" src="./images/pages/Home/Icon_halter_dumbbell.svg" alt="Halteres" />
-              <h2 className="titulo_bloco_sobre">Estrutura Completa e Moderna</h2>
-              <p>Equipamentos de última geração e ambientes projetados para o seu conforto e desempenho.</p>
-            </div>
-          </SwiperSlide>
+        <div className="bloco_mais_sobre">
+          <img className="img_icon_mais_sobre" src="./images/pages/Home/Icon_halter_dumbbell.svg" alt="Halteres" />
+          <h2 className="titulo_bloco_sobre">Estrutura Completa e Moderna</h2>
+          <p>Equipamentos de última geração e ambientes projetados para o seu conforto e desempenho.</p>
+        </div>
 
-          <SwiperSlide>
-            <div className="bloco_mais_sobre">
-              <img className="img_icon_mais_sobre" src="./images/pages/Home/Icon_halter.svg" alt="Halter" />
-              <h2 className="titulo_bloco_sobre">Variedade de Modalidades</h2>
-              <p>Desde musculação até pilates e artes marciais, temos aulas para todos os gostos e objetivos.</p>
-            </div>
-          </SwiperSlide>
+        <div className="bloco_mais_sobre">
+          <img className="img_icon_mais_sobre" src="./images/pages/Home/Icon_halter.svg" alt="Halter" />
+          <h2 className="titulo_bloco_sobre">Variedade de Modalidades</h2>
+          <p>Desde musculação até pilates e artes marciais, temos aulas para todos os gostos e objetivos.</p>
+        </div>
 
-          <SwiperSlide>
-            <div className="bloco_mais_sobre">
-              <img className="img_icon_mais_sobre" src="./images/pages/Home/Icon_halter_lista.svg" alt="Lista de exercícios" />
-              <h2 className="titulo_bloco_sobre">Planos Flexíveis para o Seu Bolso</h2>
-              <p>Escolha o plano que melhor se adapta à sua rotina, com total transparência.</p>
-            </div>
-          </SwiperSlide>
-        </Swiper>
+        <div className="bloco_mais_sobre">
+          <img className="img_icon_mais_sobre" src="./images/pages/Home/Icon_halter_lista.svg" alt="Lista de exercícios" />
+          <h2 className="titulo_bloco_sobre">Planos Flexíveis para o Seu Bolso</h2>
+          <p>Escolha o plano que melhor se adapta à sua rotina, com total transparência.</p>
+        </div>
       </section>
 
       <section className="unidade">
         <div className="div_unidade">
-          <div>
-            <p>Venha Conhecer nossa Unidade</p>
+          <div className="titulo_texto_unidade">
+            <h2 className="titulo_unidade">Venha Conhecer nossa Unidade</h2>
+            <p className="texto_unidade">
+              Descubra o espaço que combina modernidade, conforto e equipamentos de alta performance para o seu melhor treino
+            </p>
+          </div>
+
+          <div className="imgs_unidade">
+            <div className="bloco_imgs_unidade">
+              <Swiper
+                modules={[Autoplay]}
+                loop={true}
+                rewind={true}
+                autoplay={{ delay: 5000, disableOnInteraction: false }}
+                speed={1000}
+                slidesPerView={1}
+                spaceBetween={10}
+                className="carrossel"
+              >
+                <SwiperSlide>
+                  <img className="img_unidade" src="./images/pages/Home/fachada_academia.jpg" alt="Fachada da academia" />
+                </SwiperSlide>
+
+                <SwiperSlide>
+                  <img className="img_unidade" src="./images/pages/Home/sala_academia.jpg" alt="Sala da academia" />
+                </SwiperSlide>
+
+                <SwiperSlide>
+                  <img className="img_unidade" src="./images/pages/Home/corredor_academia.jpg" alt="Corredor da academia" />
+                </SwiperSlide>
+
+                <SwiperSlide>
+                  <img className="img_unidade" src="./images/pages/Home/canto_academia.jpg" alt="Canto da academia" />
+                </SwiperSlide>
+              </Swiper>
+
+              <div className="texto_local">
+                <Link to="/">
+                  <img className="img_icon_local" src="./images/pages/Home/icon_local.svg" alt="Ícone de localização" />
+                  <h2 className="nome_cidade_unidade">Metrópolis</h2>
+                  <p className="nome_rua_unidade">Clinton Street, 344</p>
+                  <p className="nome_rua_unidade">New Troy, Metrópolis - EUA</p>
+                </Link>
+              </div>
+
+              <Link to="/">
+                <p className="texto_matricula">Matricule-se</p>
+              </Link>
+              <Link to="/">
+                <p className="texto_ver_mais">Ver Mais</p>
+              </Link>
+            </div>
           </div>
         </div>
       </section>
-    </main>
+    </main >
   );
 }
 
